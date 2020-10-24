9bbe21076970006d2598c16260d7a9cae977d591
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2020-10-24 16:07:38 -0300

    Uhuuu

--- desenvolvimento_a_parte/log/log.py
+++ desenvolvimento_a_parte/log/log.py
@@ -1,5 +1,5 @@
-log = "É hora de morfar"
+from pathlib import Path
 
-with open('desenvolvimento_a_parte\log\log.txt', 'w') as arquivo:
-    arquivo.write(str(log))
-
+fileName = r"C:\Users\luisk\Documents\MeuProjetos\Um_lance_ai_vc_n_entenderia\desenvolvimento_a_parte\log\log.txt"
+fileObj = Path(fileName)
+print(fileObj.is_file())
