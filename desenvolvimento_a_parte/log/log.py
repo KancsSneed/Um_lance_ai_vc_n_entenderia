885f941b7addd12541964ac160225a63c63d1412
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2020-10-24 16:20:54 -0300

    Consigui hihihi

--- desenvolvimento_a_parte/log/log.py
+++ desenvolvimento_a_parte/log/log.py
@@ -1,5 +1,15 @@
 from pathlib import Path
+ 
+log = 'Pois é irmão'
 
 fileName = r"C:\Users\luisk\Documents\MeuProjetos\Um_lance_ai_vc_n_entenderia\desenvolvimento_a_parte\log\log.txt"
 fileObj = Path(fileName)
-print(fileObj.is_file())
+fileObj.is_file()
+
+if fileObj.is_file() == True:
+    with open('log.txt', 'r') as arquivo:
+        for valor in arquivo:
+            print(valor)
+else:
+    with open('log.txt', 'w') as arquivo:
+        arquivo.write(str(log))
